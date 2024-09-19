526e130db2cc89cf3323b9d48cd97f8bbe38d6ea
Armando Valdez <155396846+valde148@users.noreply.github.com>
2024-09-19 13:18:08 -0500

    making main method

--- main.java
+++ main.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+class main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter about of Progamers: ");
+        int pro = s.nextInt();
+
+        int[][] progamerprefernces = new int[pro][pro];
+        int[][] companyPrefernces = new int[pro][pro];
+
+        for(int i = 0; i < pro; i++){
+            System.out.println("Insert prefrences for programmer  " + i );
+            for(int j = 0; j < pro; j++){
+                progamerprefernces [i][j] = s.nextInt();
+            }
+        }
+        for(int x = 0; x < pro; x++){
+            System.out.println("Insert prefrences for company " + i);
+            for(int y = 0; y <pro; y++){
+                companyPrefernces[x][y] = s.nextInt();
+            }
+        }
+
+        
+
+
+    }
+}
